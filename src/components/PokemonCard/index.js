4f5fa5fc12e4b99ba919bc68646c034c52e1f104
John <yevhenii.bakhmat@nure.ua>
2021-09-13 17:56:35 +0300

    Task #1 Use classnames everywhere

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -1,6 +1,7 @@
 import s from "./style.module.css";
 import cardBack from "./assets/card-back-side.jpg";
 import { useState } from "react";
+import cn from "classnames";
 const PokemonCard = ({ img, name, stats, type, values, id }) => {
   const [isActive, setActive] = useState(false);
   const handleClick = () => {
@@ -8,15 +9,15 @@ const PokemonCard = ({ img, name, stats, type, values, id }) => {
   };
   return (
     <div className={s.root} onClick={handleClick}>
-      <div className={`${s.pokemonCard} ${isActive && s.active}`}>
+      <div className={`${cn(s.pokemonCard, {[s.active] : isActive})}`}>
         <div className={s.cardFront}>
-          <div className={`${s.wrap} ${s.front}`}>
-            <div className={`${s.pokemon} ${s[type]}`}>
+          <div className={`${cn(s.wrap , s.front)}`}>
+            <div className={`${cn(s.pokemon, s[type])}`}>
               <div className={s.values}>
-                <div className={`${s.count} ${s.top}`}>{values.top}</div>
-                <div className={`${s.count} ${s.right}`}>{values.right}</div>
-                <div className={`${s.count} ${s.bottom}`}>{values.bottom}</div>
-                <div className={`${s.count} ${s.left}`}>{values.left}</div>
+                <div className={`${cn(s.count, s.top)}`}>{values.top}</div>
+                <div className={`${cn(s.count, s.right)}`}>{values.right}</div>
+                <div className={`${cn(s.count, s.bottom)}`}>{values.bottom}</div>
+                <div className={`${cn(s.count, s.left)}`}>{values.left}</div>
               </div>
               <div className={s.imgContainer}>
                 <img src={img} alt={name} />
@@ -33,7 +34,7 @@ const PokemonCard = ({ img, name, stats, type, values, id }) => {
         </div>
 
         <div className={s.cardBack}>
-          <div className={`${s.wrap} ${s.back}`}>
+          <div className={`${cn(s.wrap, s.back)}`}>
             <img src={cardBack} alt="Сard Backed" />
           </div>
         </div>
