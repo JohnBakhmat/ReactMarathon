acc9fd8b5d3615721145633e61108268bf90843f
John <yevhenii.bakhmat@nure.ua>
2021-09-16 19:28:49 +0300

    About page

--- src/routes/AboutPage/index.js
+++ src/routes/AboutPage/index.js
@@ -1,7 +1,20 @@
+
+import AboutLink from '../../components/AboutLink'
+import face from './assets/face.jpg'
+import s from './styles.module.css'
 function AboutPage() {
 	return (
 		<div>
-			<h1>This is AboutPage</h1>
+			<div className={s.wrap}>
+				<img className={s.face} src={face} alt="My face"/>
+				<div className={s.textBlock}>
+					<h1 className={s.name}>Yevhenii Bakhmat</h1>
+					<h1>Kharkiv National University of Radioelectronics</h1>
+					<AboutLink path={"https://github.com/Yevhenii-Bakhmat"} label="GitHub">
+						<i class="fab fa-github-square"></i>
+					</AboutLink>
+				</div>
+			</div>
 		</div>
 	)
 }
